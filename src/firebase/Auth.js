78c4bc9a96d2fda481f8c3b40afc38b12d626a8c
Mdsaifaliii <your-saifali79873@gmail.com>
2025-06-15 17:47:21 +0530

    fix: correct import casing for firebase

--- src/firebase/Auth.js
+++ src/firebase/Auth.js
@@ -1,5 +1,5 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { auth, db } from "./firebase/Firebase"; 
+import { auth, db } from "./firebase"; 
 import { setDoc, doc, getDoc } from "firebase/firestore";
 
 // Sign up
