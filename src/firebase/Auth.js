8be94d238f1b1dc78f4bb7f76b291a7bea21d3f5
Mdsaifaliii <your-saifali79873@gmail.com>
2025-06-15 18:03:50 +0530

    https://github.com/Mdsaifaliii/Travel-Go

--- src/firebase/Auth.js
+++ src/firebase/Auth.js
@@ -1,5 +1,5 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { auth, db } from "../firebase/firebase"; 
+import { auth, db } from "./Firebase"; 
 import { setDoc, doc, getDoc } from "firebase/firestore";
 
 // Sign up
