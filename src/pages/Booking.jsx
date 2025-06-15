2b70cddc035f7bdcaeeb306449e4ead6382d9504
Mdsaifaliii <your-saifali79873@gmail.com>
2025-06-15 17:59:23 +0530

    https://github.com/Mdsaifaliii/Travel-Go

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { db } from "../Firebase";
+import { db } from "../firebase/firebase";
 import { collection, addDoc } from "firebase/firestore";
 import BookingConfirmationModal from "./BookingConfirmationModal";
 
